77a41c66b6552a8e6bfdaf86ede7454a332bac35
dhyeyppatel <dhyey@appifly.com>
2026-01-30 11:30:00 +0530

    Week 07 - Web Framework Architecture, Django MVT, URL Routing

---
+++ practice/week07_django_basics/urls_example.py
@@ -0,0 +1,12 @@
+# Week 07 - Django URL Routing Practice
+# from django.urls import path
+# Stockify URL patterns (study example)
+urlpatterns_example = [
+    # ("", "home_view", "home"),
+    # ("products/", "product_list_view", "product_list"),
+    # ("products/<int:pk>/", "product_detail_view", "product_detail"),
+    # ("products/add/", "add_product_view", "add_product"),
+    # ("login/", "login_view", "login"),
+    # ("register/", "register_view", "register"),
+]
+# Django MVT flow: URL -> View -> Template -> HTTP Response
